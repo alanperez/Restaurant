6d5b763ad46606601b8e10c15c8a22fcb3dc5a7e
Alan Perez <alanp9six@gmail.com>
2020-03-06 23:02:28 -0600

    :sparkles: Autoprefixer
    Imported vendor autoprefixer for our csss

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function() {
   // location of scss files
   return gulp.src('./assets/scss/**/*.scss')
   // run sass on the files
   .pipe(sass().on('error', sass.logError))
+  // this will add extra prefixes to the converted css files 
+  .pipe(autoprefixer({
+    browsers: ['last 2 versions']
+  }))
   // export to destination '/css'
   .pipe(gulp.dest('./public/css'))
 });
