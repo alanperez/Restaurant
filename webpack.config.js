03eace678d5575288d560e7d7e6889478cdf3fb4
Alan Perez <alanp9six@gmail.com>
2020-03-08 20:31:51 -0500

    setting up rules

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,26 @@ module.exports = {
   },
 
   output: { filename: '[name].js',
-  path: path.resolver(__dirname)  },
+  path: path.resolver(__dirname,  'public/js/components')  },
   
-  moodule: {
-    
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'prettier-loader']
+      },
+      { test: /\.svelte$/, exclude: /node_modules/, use: 'svelte-loader' },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+          'postcss-loader'
+        ]
+      }
+    ]
   }
 }
